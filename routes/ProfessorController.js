4f0dcb139d7cb9fd580d0f10be2399d4c0d56e45
MatheusMb7 <matheus.bruno@iesb.edu.br>
2025-09-22 21:23:50 -0300

    Finalizando o ProfessorControlller

--- routes/ProfessorController.js
+++ routes/ProfessorController.js
@@ -28,10 +28,10 @@ if(!nome|| !cpf || !email || !disciplina || !curso)    {
 }
 
 const professor = professores.find (professor => professor.cpf == cpf)
-if(professores){
+if(professor){
     return res.status(409).json({error: "CPF já cadastrado"})
 }
-// cadastrar a nova pessoa na lista
+// cadastrar o novo professor na lista
 const novoProfessor = {
     id: Date.now(),
     nome,
@@ -40,20 +40,20 @@ const novoProfessor = {
     disciplina,
     curso
 }
-// inserir a nova pessoa montada na lista
-pessoas.push(novoProfessor)
+// inserir o novo Professor montada na lista
+professores.push(novoProfessor)
 res.status(201).json({message: "Professor Cadastrado!!!!", novoProfessor})
 
 })
 
 // Listar todos
-// - GET /pessoas
+// - GET /professores
 router.get('/professores', (req, res, next) => {
     res.json(professores)
 })
 
 // Buscar um
-// - GET /pessoas/{id}
+// - GET /professores/{id}
 router.get('/professores/:id', (req, res, next) => {
     const idRecebido = req.params.id
     const professor = professores.find(professor => professor.id == idRecebido)
@@ -65,19 +65,19 @@ router.get('/professores/:id', (req, res, next) => {
 })
 
 // atualizar
-// - PUT /pessoas/{id}
+// - PUT /professores/{id}
 router.put('/professores/:id', (req, res, next) => {
     const {nome, email, disciplina, curso} = req.body
-if(!nome || !email || !disciplina || curso){
+if(!nome || !email || !disciplina || !curso){
     return res.status(400).json({error: "nome, email , disciplina, e curso são obrigatorios"})
 }
-// validar se a pessoa com aquele ID existe na lista
+// validar se o professor com aquele ID existe na lista
 const idRecebido = req.params.id
 const professor = professores.find(professor => professor.id == idRecebido)
 if(!professor){
-    return res.status(404).json({error: "pessoa não encontrado"})
+    return res.status(404).json({error: "Professor não encontrado"})
 }
-// sobreescreve os dados das pessoas pra atualizar
+// sobreescreve os dados dos professores pra atualizar
 professor.nome = nome
 professor.email = email
 professor.disciplina = disciplina
@@ -86,14 +86,14 @@ res.json({message: "Professor atualizado com sucesso"})
 })
 
 // deletar
-// - DELETE /pessoas/{id}
+// - DELETE /professores/{id}
 router.delete('/professores/:id', (req, res, next) => {
     const idRecebido = req.params.id
     const professor = professores.find(professor => professor.id == idRecebido)
     if (!professor) {
         return res.status(404).json({error:"Professor não encontrado"})
     }
-    professor = professores.filter(professor => professor.id != idRecebido)
+    professores = professores.filter(professor => professor.id != idRecebido)
 
     res.json({message: "Professor excluido com sucesso"})
 })
